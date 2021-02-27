50e4a640c837af647b5b0a3944275d2696038125
padmashrimallapur <padmashri.mallapur@gmail.com>
2021-02-26 23:43:21 -0500

    Fragments with jetpack compose. Also demonstrated how we can add the android view using composable.

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.fragment.app.FragmentManager
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,58 +26,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setContent {
-            ScrollableColumn(
-                modifier =
-                Modifier
-                    .fillMaxSize()
-                    .background(color = Color(0xFFEEECEC))
-            )
-            {
-                Image(
-                    bitmap = imageFromResource(
-                        res = resources,
-                        resId = R.drawable.happy_meal_small
-                    ),
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop,
-                )
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Row(modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-
-                    ) {
-                        Text(
-                            text = "Happy Meal",
-                            style = TextStyle(
-                                fontSize = TextUnit.Companion.Sp(26)
-                            )
-                        )
-                        Text(
-                            text = "$5.99",
-                            style = TextStyle(
-                                fontSize = TextUnit.Companion.Sp(18),
-                                color = Color(0xFF85B453)
-                            ),
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                    }
-
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(
-                        text = "800 Calories",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(17)
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(top = 20.dp))
-                    Button(onClick = { },
-                    modifier = Modifier.align(Alignment.CenterHorizontally)
-                        ) {
-                        Text(text = "ORDER NOW")
-                    }
-                }
-            }
-        }
+       supportFragmentManager.beginTransaction()
+           .replace(R.id.main_container, FragmentList()).commit()
     }
 }
